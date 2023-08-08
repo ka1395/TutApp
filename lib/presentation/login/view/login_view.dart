06795e2cac6fa27957d5aa0619468cbd99efd69f
ka1395 <ka1395@fayoum.ed.eg>
2023-08-08 14:29:27 +0300

    finished ui login

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tutapp/presentation/login/viewmodel/login_viewmodel.dart';
 
+import '../../resources/assets_manager.dart';
+import '../../resources/color_manager.dart';
+import '../../resources/strings_manager.dart';
+import '../../resources/values_manager.dart';
+
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
@@ -13,6 +18,7 @@ class _LoginViewState extends State<LoginView> {
  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
  final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start();
@@ -34,7 +40,83 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Container();
   }
+Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                    child: Image(image: AssetImage(ImageAssets.splashLogo))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsUserNameValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _userNameController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.username,
+                              labelText: AppStrings.username,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.usernameError),
+                            );
+                      }),
+                )
+                , const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsPasswordValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                            keyboardType: TextInputType.visiblePassword,
+                            controller: _passwordController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.password,
+                              labelText: AppStrings.password,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.passwordError),
+                            );
+                      }),
+                ),
+                  const SizedBox(
+                  height: AppSize.s28,
+                ),
 
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outIsPasswordValid,
+                      builder: (context, snapshot) {
+                        return ElevatedButton(onPressed: (){
+                          _viewModel.login();
+                        }, child: const Text(AppStrings.login));
+                      }),
+                ),
+              
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
   @override
   void dispose() {
     _viewModel.dispose();
